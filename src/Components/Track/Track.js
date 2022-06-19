2912054dbb9e3c01edbedee34068730b028795e9
Gardego5 <garrett.davis.7777@gmail.com>
2022-06-19 16:37:49 -0700

    Use RenderAction (Removing Duplication)

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -12,9 +12,9 @@ export default class Track extends React.Component {
 
   renderAction() {
     if (this.props.isRemoval) {
-      return <button className="Track-action">-</button>;
+      return <button className="Track-action" onClick={ this.removeTrack } >-</button>; 
     } else {
-      return <button className="Track-action">+</button>;
+      return <button className="Track-action" onClick={ this.addTrack }>+</button>;
     }
   }
 
@@ -28,13 +28,6 @@ export default class Track extends React.Component {
 
   render() {
     const track = this.props.track;
-    let button;
-
-    if (this.props.isRemoval) {
-      button = <button className="Track-action" onClick={ this.removeTrack } >-</button>; 
-    } else {
-      button = <button className="Track-action" onClick={ this.addTrack }>+</button>;
-    }
 
     return (
       <div className="Track">
@@ -42,7 +35,7 @@ export default class Track extends React.Component {
           <h3>{ track.name }</h3>
           <p>{ track.artist } | { track.album }</p>
         </div>
-        { button }
+        { this.renderAction() }
       </div>
     );
   }
