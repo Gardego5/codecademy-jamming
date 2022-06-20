def3cb2b4651b1b7b3b05d93e7e9a6a91ff28694
Gardego5 <garrett.davis.7777@gmail.com>
2022-06-20 14:26:59 -0700

    Search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,46 +6,16 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist'
 
+import Spotify from '../../util/Spotify';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      results: [
-        {
-          name: 'Imagine',
-          artist: 'John Lennon',
-          album: 'Imagine',
-          id: '000',
-        },
-        {
-          name: 'Stariway To Heaven',
-          artist: 'Rodrigo y Gabriela',
-          album: 'Rodrigo y Gabriela',
-          id: '001',
-        },
-        {
-          name: 'Tamacun',
-          artist: 'Rodrigo y Gabriela',
-          album: 'Rodrigo y Gabriela',
-          id: '002',
-        }
-      ],
+      results: [],
       playlistName: 'Medley',
-      playlistTracks: [
-        {
-          name: 'Here I Go Again',
-          artist: 'Whitesnake',
-          album: 'Saints & Sinners',
-          id: '003',
-        },
-        {
-          name: 'Für Elise',
-          artist: 'Richard Clayderman',
-          album: 'Träumereien: Die Schönsten Klavier-Melodien Mit Richard Clayderman',
-          id: '004',
-        }
-      ]
+      playlistTracks: []
     };
 
     this.addTrack = this.addTrack.bind(this);
@@ -82,8 +52,8 @@ export default class App extends React.Component {
     console.log(trackURIs);
   }
 
-  search(term) {
-    console.log(term);
+  async search(term) {
+    this.setState({results: await Spotify.search(term)});
   }
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,9 @@
-const client_id = '6237df6cd3a84bd6967a03907b75660d';
-const redirect_uri = 'http://localhost:3000/';
+const clientID = '6237df6cd3a84bd6967a03907b75660d';
+const redirectURI = 'http://localhost:3000/';
 
-let userAccessToken;
+let userAccessToken = null;
 
-export default Spotify = {
+const Spotify = {
   getAccessToken() {
     const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
@@ -11,14 +11,47 @@ export default Spotify = {
     if (userAccessToken !== null) {
       return userAccessToken;
     } else if (accessTokenMatch && expiresInMatch) {
-      accessToken = accessTokenMatch[1];
+      userAccessToken = accessTokenMatch[1];
 
       let expiresIn = Number(expiresInMatch[1]);
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
-      return accessToken;
-    } else if (!accessTokenMatch && !expiresInMatch) {
+      return userAccessToken;
+    } else if (!userAccessToken && !expiresInMatch) {
       window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
     }
+  },
+  search(term) {
+    const endpoint = 'https://api.spotify.com/v1/search?type=track&q=';
+    const accessToken = Spotify.getAccessToken();
+
+    return fetch(endpoint + term, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+    })
+    .then(jsonResponse => {
+      console.log(jsonResponse)
+      if(!jsonResponse) {
+        return [];
+      } else {
+        return jsonResponse.tracks.items.map(track => {
+          return {
+            id: track.id,
+            name: track.name,
+            artist: track.artists[0].name,
+            album: track.album.name,
+            uri: track.uri,
+          }
+        });
+      }
+    })
   }
 }
+
+export default Spotify;
